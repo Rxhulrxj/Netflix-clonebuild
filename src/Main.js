c8c1a7d976ae1392b31671e1467ddeaddf7bc1d2
Rahul Raj N <88202340+Rxhulrxj@users.noreply.github.com>
2021-11-29 17:53:25 +0530

    fixed some known bugs

--- src/Main.js
+++ src/Main.js
@@ -1,70 +1,71 @@
-import React,{useEffect} from 'react';
-import App from './screens/App';
-import "./main.css"; 
-import {auth} from './firebase';
-import Login from './screens/login';
-import Profile from './screens/Profile';
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
-import {login ,logout ,selectUser} from "./features/counter/userSlice";
-import Movie from './screens/Movie';
-import Tv from './screens/Tv';
+import React, { useEffect } from "react";
+import App from "./screens/App";
+import "./main.css";
+import { auth } from "./firebase";
+import Login from "./screens/login";
+import Profile from "./screens/Profile";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./features/counter/userSlice";
+import Movie from "./screens/Movie";
+import Tv from "./screens/Tv";
 import Spinner from "react-spinkit";
-import {useAuthState} from "react-firebase-hooks/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
 
-  function Main() {
-    //   const user = useSelector(selectUser);
-      const dispatch = useDispatch();
-      const[user ,loading] = useAuthState(auth);
-      useEffect(() => {
-          const unsubscribe =auth.onAuthStateChanged((userAuth) =>{
-              if(userAuth){
-                  dispatch(
-                      login({
-                      uid: userAuth.uid,
-                      email: userAuth.email,
-                  })
-                );
-              }
-              else{
-                  dispatch(logout());
-              }
-          });
-          return unsubscribe;
-      },[dispatch]);
-      if(loading) {
-        return(
-          <div className="AppLoading">
-            <div className="AppLoadingContents">
-            <Spinner name="line-scale-pulse-out-rapid" color="purple" fadeIn="none"/>
-            </div>
-          </div>
-          
-        )
+function Main() {
+  const dispatch = useDispatch();
+  const [user, loading] = useAuthState(auth);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        dispatch(logout());
       }
-      return(
-          <div className="MAIN">
-              <Router>
-                  {!user ? (
-                      <Login />
-                  ):(
-                  <Switch>
-                      <Route exact path = '/profile'>
-                          <Profile />
-                      </Route>
-                      <Route exact path = '/Movie'>
-                          <Movie />
-                      </Route>
-                      <Route exact path = '/TV'>
-                          <Tv />
-                      </Route>
-                      <Route exact path="/">
-                      <App />
-                      </Route>
-                    </Switch>
-                    )}
-                </Router>
-          </div>
-      );
+    });
+    return unsubscribe;
+  }, [dispatch]);
+  if (loading) {
+    return (
+      <div className="AppLoading">
+        <div className="AppLoadingContents">
+          <Spinner
+            name="line-scale-pulse-out-rapid"
+            color="purple"
+            fadeIn="none"
+          />
+        </div>
+      </div>
+    );
   }
-  export default Main;
+  return (
+    <div className="MAIN">
+      <Router>
+        {!user ? (
+          <Login />
+        ) : (
+          <Switch>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+            <Route exact path="/Movie">
+              <Movie />
+            </Route>
+            <Route exact path="/TV">
+              <Tv />
+            </Route>
+            <Route exact path="/">
+              <App />
+            </Route>
+          </Switch>
+        )}
+      </Router>
+    </div>
+  );
+}
+export default Main;
--- src/screens/App.js
+++ src/screens/App.js
@@ -1,24 +1,50 @@
-import {originals,action,Horror,Comedy, Romance, documentaries,populartv,tvonair} from '../urls'
-import '../screens/App.css';
-import Banner from '../Components/Banner/Banner';
-import NavBar from '../Components/NavBar/NavBar';
-import Rowpost from '../Components/Rowpost/Rowpost';
-import Footer from './Footer';
+import {
+  originals,
+  action,
+  Horror,
+  Comedy,
+  Romance,
+  documentaries,
+  populartv,
+  tvonair,
+} from "../urls";
+import "../screens/App.css";
+import Banner from "../Components/Banner/Banner";
+import NavBar from "../Components/NavBar/NavBar";
+import Rowpost from "../Components/Rowpost/Rowpost";
+import Footer from "./Footer";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase";
+import Spinner from "react-spinkit";
 
 function App() {
+  const [user, loading] = useAuthState(auth);
+  if (loading) {
+    return (
+      <div className="AppLoading">
+        <div className="AppLoadingContents">
+          <Spinner
+            name="line-scale-pulse-out-rapid"
+            color="purple"
+            fadeIn="none"
+          />
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="App">
-     <NavBar/>
-     <Banner/>
-     <Rowpost  url={originals} title="Netflix Originals"    />
-     <Rowpost url={populartv} title="Popular Series" isSmall />
-     <Rowpost url={tvonair} title="TV Shows OnAir" isSmall />
-     <Rowpost url={action} title="Action Movies" isSmall />
-     <Rowpost url={Horror} title="Horror Movies" isSmall />
-     <Rowpost url={Comedy} title="Comedy Movies" isSmall />
-     <Rowpost url={Romance} title="Romantic Movies" isSmall />
-     <Rowpost url={documentaries} title="Documentaries" isSmall />
-     <Footer />
+      <NavBar />
+      <Banner />
+      <Rowpost url={originals} title="Netflix Originals" />
+      <Rowpost url={populartv} title="Popular Series" isSmall />
+      <Rowpost url={tvonair} title="TV Shows OnAir" isSmall />
+      <Rowpost url={action} title="Action Movies" isSmall />
+      <Rowpost url={Horror} title="Horror Movies" isSmall />
+      <Rowpost url={Comedy} title="Comedy Movies" isSmall />
+      <Rowpost url={Romance} title="Romantic Movies" isSmall />
+      <Rowpost url={documentaries} title="Documentaries" isSmall />
+      <Footer />
     </div>
   );
 }
